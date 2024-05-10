9ce9a4b49dc3943617d747fae46ec4647a96065c
hukuryo <ryohei.1998.05.09@gmail.com>
2024-05-10 22:55:52 +0900

    新しいhooksを試すコンポーネントを作成

---
+++ src/components/NewHooksDemo.tsx
@@ -0,0 +1,21 @@
+import { useState, useTransition } from "react";
+
+export default function NewFooksDemo() {
+  const [name, setName] = useState("");
+  const [error, setError] = useState(null);
+  const [isPending, startTransition] = useTransition();
+
+  const handleSubmit = async () => {
+    console.log("click");
+  };
+
+  return (
+    <div>
+      <input value={name} onChange={(event) => setName(event.target.value)} />
+      <button onClick={handleSubmit} disabled={isPending}>
+        Update
+      </button>
+      {error && <p>{error}</p>}
+    </div>
+  );
+}
