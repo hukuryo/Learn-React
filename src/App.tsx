8df515c1860abbed60f62ba28517fee7ab25bf38
hukuryo <ryohei.1998.05.09@gmail.com>
2025-01-03 21:18:08 +0900

    MantineでCodeHighlightを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import "@mantine/core/styles.css";
+
+import { MantineProvider } from "@mantine/core";
 import { useState } from "react";
 import { TestComponent } from "./components/TestComponent";
+import { CodeHighlightZone } from "./components/CodeHighlight";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <TestComponent name={"Taro"} onClick={() => console.log("click!")} />
-      <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
+      <MantineProvider>
+        <TestComponent name={"Taro"} onClick={() => console.log("click!")} />
+        <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
+        <CodeHighlightZone />
+      </MantineProvider>
     </>
   );
 }
